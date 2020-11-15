e9d3d412a056f656a9366b828d3c6f76341b859c
Naoya Ugajin <improve-my-level@softbank.ne.jp>
2020-11-15 04:18:29 +0000

    kadai03 commit

--- kadai03.php
+++ kadai03.php
@@ -12,16 +12,16 @@ if($name){
 #課題2
 <?php
 $total = 0;
-echo $total;
-for ($i = 0; $i <= 10000; $i++){
- echo $i;
+for ($i = 1; $i <= 10000; $i++){
+ $total += $i;
 }
+
+echo $total;
 
 ?>
 
 #課題3
 <?php
-echo $total;
 $fruits = array("orange", "banana", "melon", "grape", "lychee");
 foreach($fruits as $fruits){
  echo "要素は" . $fruits;
@@ -42,6 +42,7 @@ for($i = $start; $i <= $end; $i++){
   // 5で割り切れたら{}内を実行する
   if($i % 5 == 0)
     echo $i;
+    echo "\n";
 }
 
 ?>
